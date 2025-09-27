879e48140d18e78d0c432381cc2faef2ad235318
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-09-27 18:51:57 -0500

    🗃️ db: Add Evento model

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from apps.users.models import Usuario
 
-# Create your models here.
+class Evento(models.Model):
+    # Enumerations
+    ESTADOS = [ 
+        ("Borrador", "Borrador"),
+        ("Enviado", "Enviado"),
+        ("Aprobado", "Aprobado"),
+        ("Rechazado", "Rechazado"),
+    ]
+
+    TIPOS = [
+        ("Ludico", "Lúdico"),
+        ("Academico", "Académico")
+    ]
+
+    # Fields
+    idEvento = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    nombre = models.CharField(max_length=100)
+    estado = models.CharField(max_length=10, choices=ESTADOS, default="Borrador")
+    tipo = models.CharField(max_length=100, choices=TIPOS)
+    fecha = models.DateField()
+    horaInicio = models.TimeField()
+    horaFin = models.TimeField()
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="eventos_creados")
+
+    def __str__(self):
+        return f"{self.nombre} - {self.tipo} ({ self.estado })"
+    
+    class Meta:
+        db_table = "eventos"
+        verbose_name = "evento"
+        verbose_name_plural = "eventos"
